7370e518c33f27087a476d8aeddb5741a171473a
uhager <uhager42@gmail.com>
2016-02-09 12:27:12 -0500

    Some cleaning up, finally put declarations into header.

--- SbHalfPong.cpp
+++ SbHalfPong.cpp
@@ -6,8 +6,8 @@ author: Ulrike Hager
 #include <string>
 #include <sstream>
 #include <stdexcept>
-#include <climits>
-#include <cmath>
+// #include <climits>
+//#include <cmath>
 #include <random>
 #include <algorithm>
 #include <functional>
@@ -21,93 +21,11 @@ author: Ulrike Hager
 #include "SbWindow.h"
 #include "SbObject.h"
 
+#include "SbHalfPong.h"
+
+
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
-
-
-class Ball;
-class Spark;
-class Paddle;
-
-class Paddle : public SbObject
-{
-public:
-  Paddle();
-  void handle_event(const SDL_Event& event);
-  int move();
-};
-
-
-
-class Ball : public SbObject
-{
-public:
-  Ball();
-  //  void handle_event(const SDL_Event& event);
-  /*! \retval 1 if ball in goal
-    \retval 0 else
-   */
-  int move(const SDL_Rect& paddleBox);
-  void render();
-  /*! Reset after goal.
-   */
-  void reset();
-  static Uint32 resetball(Uint32 interval, void *param );
-  Uint32 remove_spark(Uint32 interval, void *param, int index );
-
-  
-private:
-  int goal_ = 0;
-  std::vector<Spark> sparks_;
-  std::default_random_engine generator_;
-  std::uniform_int_distribution<int> distr_number { 15, 30 };
-  std::normal_distribution<double> distr_position { 0.0, 0.01 };
-  std::normal_distribution<double> distr_size { 0.003, 0.002 };
-  std::uniform_int_distribution<int> distr_lifetime { 100, 400 };
-
-  void create_sparks();
-  void center_in_front(const SDL_Rect& paddleBox);
-  void delete_spark(int index);
-};
-
-
-
-class Spark : public SbObject
-{
-  friend class Ball;
-public:
-  Spark(double x, double y, double width, double height);
-  ~Spark();
-
-  static Uint32 expire(Uint32 interval, void* param);
-
-  void set_texture(SbTexture* tex) {texture_ = tex;}
-  int index() { return index_;}
-  bool is_dead() {return is_dead_;}  
-  Uint32 lifetime() { return lifetime_;}
-
-private:
-  //  SDL_TimerID spark_timer_;
-  int index_ = 0;
-  bool is_dead_ = false;
-  Uint32 lifetime_ = 100;
-};
-
-
-  
-class Message : public SbObject
-{
-public:
-  Message(double x, double y, double width, double height);
-  ~Message();
-
-  void set_font(TTF_Font* font){ font_ = font;}
-  void set_text(std::string text);
-
-private:
-  TTF_Font* font_ = nullptr;
-  SDL_Color color_{210, 160, 10, 0};
-};
 
 
 
@@ -391,9 +309,10 @@ Message::~Message()
 
 
 
-
+/////  globals /////
 SbWindow* SbObject::window;
 TTF_Font *fps_font = nullptr;
+
 
 int main()
 {
@@ -401,6 +320,7 @@ int main()
     SbWindow window;
     window.initialize("Half-Pong", SCREEN_WIDTH, SCREEN_HEIGHT);
     SbObject::window = &window ;
+    std::vector<SbObject*> objects;
     Paddle paddle;
     Ball ball;
     
@@ -429,6 +349,12 @@ int main()
 
     int frame_counter = 0;
     fps_timer.start();
+
+    objects.push_back(&paddle);
+    objects.push_back(&ball);
+    objects.push_back(&fps_counter);
+    objects.push_back(&goals);
+    objects.push_back(&game_over);
     
     while (!quit) {
       while( SDL_PollEvent( &event ) ) {
@@ -446,11 +372,8 @@ int main()
 	  }
 	}
 	window.handle_event(event);
-	paddle.handle_event(event);
-	ball.handle_event( event );
-	fps_counter.handle_event( event );
-	goals.handle_event( event );
-	game_over.handle_event( event );
+	std::for_each( objects.begin(), objects.end(),
+		       [event] (SbObject* obj) {obj->handle_event( event );} );
       }
       // move objects   
       if ( goal_counter > 0 ) {
---
+++ SbHalfPong.h
@@ -0,0 +1,99 @@
+/* SbHalfPong.h
+author: Ulrike Hager
+*/
+
+#include <random>
+#include <string>
+
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+#include "SbObject.h"
+
+
+class Ball;
+class Spark;
+class Paddle;
+
+class Paddle : public SbObject
+{
+public:
+  Paddle();
+  void handle_event(const SDL_Event& event);
+  int move();
+};
+
+
+
+class Ball : public SbObject
+{
+public:
+  Ball();
+  //  void handle_event(const SDL_Event& event);
+  /*! \retval 1 if ball in goal
+    \retval 0 else
+   */
+  int move(const SDL_Rect& paddleBox);
+  void render();
+  /*! Reset after goal.
+   */
+  void reset();
+  static Uint32 resetball(Uint32 interval, void *param );
+  Uint32 remove_spark(Uint32 interval, void *param, int index );
+
+  
+private:
+  int goal_ = 0;
+  std::vector<Spark> sparks_;
+  std::default_random_engine generator_;
+  std::uniform_int_distribution<int> distr_number { 15, 30 };
+  std::normal_distribution<double> distr_position { 0.0, 0.01 };
+  std::normal_distribution<double> distr_size { 0.003, 0.002 };
+  std::uniform_int_distribution<int> distr_lifetime { 100, 400 };
+
+  void create_sparks();
+  void center_in_front(const SDL_Rect& paddleBox);
+  void delete_spark(int index);
+};
+
+
+
+class Spark : public SbObject
+{
+  friend class Ball;
+public:
+  Spark(double x, double y, double width, double height);
+  ~Spark();
+
+  static Uint32 expire(Uint32 interval, void* param);
+
+  void set_texture(SbTexture* tex) {texture_ = tex;}
+  int index() { return index_;}
+  bool is_dead() {return is_dead_;}  
+  Uint32 lifetime() { return lifetime_;}
+
+private:
+  //  SDL_TimerID spark_timer_;
+  int index_ = 0;
+  bool is_dead_ = false;
+  Uint32 lifetime_ = 100;
+};
+
+
+  
+class Message : public SbObject
+{
+public:
+  Message(double x, double y, double width, double height);
+  ~Message();
+
+  void set_font(TTF_Font* font){ font_ = font;}
+  void set_text(std::string text);
+
+private:
+  TTF_Font* font_ = nullptr;
+  SDL_Color color_{210, 160, 10, 0};
+};
+
+
